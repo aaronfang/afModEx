dd82ed8b5f1721eef6c4417392762e0002d384f2
Aaron Fang <aaronfang@users.noreply.github.com>
2016-02-04 14:28:49 +0800

    add comments

--- core/mathLibs.py
+++ core/mathLibs.py
@@ -1,3 +1,14 @@
+'''
+This class definds several methords to make the math easier. 
+
+methords:
+distance
+
+data
+'''
+
+import maya.cmds as mc
+
 class _math(object):
 	def __init__(self):
 		pass
