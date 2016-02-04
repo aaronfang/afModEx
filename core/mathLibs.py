03982cb5e5d431a7ab209ed6647bdfb9b05bb9b2
Aaron Fang <fangzheng003@gmail.com>
2016-02-04 20:35:38 +0800

    update scale uv ratio func.

--- core/mathLibs.py
+++ core/mathLibs.py
@@ -1,5 +1,5 @@
 '''
-This class definds several methords to make the math easier. 
+This class definds several methords to make the math easier.
 
 methords:
 distance
@@ -22,11 +22,27 @@ UDIM
 
 '''
 
-import maya.cmds as mc
+import maya.cmds as cmds
 
 class _math(object):
 	def __init__(self):
 		pass
-	
+
 	def distance(self,point1,point2):
-		
+		pass
+
+    def scale_uv_by_ratio(self,tex_res,ratio):
+        multi = 1
+        cur_unit = cmds.currentUnit(q=True,f=True)
+        if not cur_unit == "centimeter":
+            if cur_unit == "meter":
+                multi = 0.01
+            elif cur_unit == "millimeter":
+                multi = 10
+            else:
+                cmds.confirmDialog(t="sorry",button="ok",message="Only work on cm/m/mm unit.")
+        multi = (multi*(8192/tex_res))/8
+		unfold=0.0009765625*(ratio)
+		scale_ratio = unfold*mult
+        
+        return scale_ratio
